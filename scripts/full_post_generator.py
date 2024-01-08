b29e5403d190dc7bd4d572c1aab4be17b73456a1
AntonWille <anton.wille@gmail.com>
2024-01-08 16:14:46 +0100

    Update script

--- scripts/full_post_generator.py
+++ scripts/full_post_generator.py
@@ -1,17 +1,13 @@
 import pandas as pd
 from sqlalchemy import create_engine, text
-import html2text
-
-def html_to_text(html):
-    text_maker = html2text.HTML2Text()
-    text_maker.ignore_links = True
-    text_maker.bypass_tables = False
-    return text_maker.handle(html)
 
 def fetch_question(engine, question_id):
-    query = f"SELECT * FROM questions WHERE question_id = {question_id}"
-    df = pd.DataFrame(engine.execute(text(query)))
-    return df.iloc[0].to_dict()
+    query = text("SELECT * FROM questions WHERE question_id = :qid")
+    result = engine.execute(query, qid=question_id).fetchone()
+    if result:
+        return dict(result)
+    else:
+        return None
 
 def fetch_answers(engine, question_id):
     query = f"SELECT * FROM answers WHERE question_id = {question_id}"
@@ -19,7 +15,11 @@ def fetch_answers(engine, question_id):
 
 def fetch_comments(engine, post_ids):
     query = f"SELECT * FROM comments WHERE commentable_id IN ({','.join(map(str, post_ids))})"
-    return pd.DataFrame(engine.execute(text(query)))
+    df = pd.DataFrame(engine.execute(text(query)))
+    if df.empty:
+        return pd.DataFrame(columns=['comment_id', 'commentable_type', 'commentable_id', 'owner_id',
+                                            'owner_display_name', 'body', 'score'])
+    return df
 
 def fetch_all_question_ids(engine):
     query = "SELECT question_id FROM questions"
@@ -30,34 +30,32 @@ def create_post_file(engine, question_id):
     question = fetch_question(engine, question_id)
     filename = f"data/html/{question['score']}{question['category']}_{question_id}.html"
     answers_df = fetch_answers(engine, question_id)
+
     post_ids = [question_id]
     if not answers_df.empty:
          post_ids += answers_df['answer_id'].tolist()
 
     comments_df = fetch_comments(engine, post_ids)
-    if comments_df.empty:
-        comments_df = pd.DataFrame(columns=['comment_id', 'commentable_type', 'commentable_id', 'owner_id',
-                                            'owner_display_name', 'body', 'score'])
 
     text = f" <h2> Title: {question['title']} </h2>"
-    text += f" <h3> {question['owner_display_name']}, question_id: {question_id} </h3>"
+    text += f" <h4> {question['owner_display_name']}, question_id: {question_id} </h4>"
     text += f"Score: {question['score']}, Tags: {question['tags']} <br>"
     text += question['body']
 
     question_comments = comments_df[comments_df['commentable_id'] == question_id]
     for _, comment in question_comments.iterrows():
-        text += f"<h4> Comment {comment['comment_id']} {comment['owner_display_name']}: </h4>"
+        text += f"<h4> {comment['owner_display_name']}, Id: {comment['comment_id']} Score: {comment['score']}: </h4>"
         text += comment['body']
         text += "<br>"
 
     for _, answer in answers_df.iterrows():
         text += "------------------------------------------------------------------ <br>"
-        text += f"<h3> Answer {answer['answer_id']} {answer['owner_display_name']}: </h3>"
+        text += f"<h3> {answer['owner_display_name']}, Id: {answer['answer_id']}, Score: {answer['score']}: </h3>"
         text += answer['body']
 
         answer_comments = comments_df[comments_df['commentable_id'] == answer['answer_id']]
         for _, comment in answer_comments.iterrows():
-            text += f"<h4> Comment {comment['comment_id']} {comment['owner_display_name']}: </h4>"
+            text += f"<h4> {comment['owner_display_name']}, Comment {comment['comment_id']} Score: {comment['score']}: </h4>"
             text += comment['body']
             text += "<br>"
 
